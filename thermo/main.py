56e0a5986c2449c80ca831722083ec394d5e3fdb
Rick R <rick@sillygoatsoftware.com>
2023-03-11 13:04:50 -0500

    first commit

---
+++ thermo/main.py
@@ -0,0 +1,15 @@
+import time
+import board
+import adafruit_ahtx0
+
+# Create sensor object, communicating over the board's default I2C bus
+i2c = board.I2C()  # uses board.SCL and board.SDA
+sensor = adafruit_ahtx0.AHTx0(i2c)
+
+f = (sensor.temperature * 1.8) + 32
+
+while True:
+    print("\nTemperature: %0.1f F" % f )
+    print("Humidity: %0.1f %%" % sensor.relative_humidity)
+    time.sleep(2)
+
